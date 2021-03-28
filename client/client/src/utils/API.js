a139679bb0582d1936b9e473ff89bcee58800a1e
Valdo <vlorian22@GMAIL.COM>
2021-03-28 12:17:16 -0500

    added npm packagaes

---
+++ client/client/src/utils/API.js
@@ -0,0 +1,57 @@
+// route to get logged in user's info (needs the token)
+export const getMe = (token) => {
+  return fetch('/api/users/me', {
+    headers: {
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const createUser = (userData) => {
+  return fetch('/api/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
+};
+
+export const loginUser = (userData) => {
+  return fetch('/api/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
+};
+
+// save book data for a logged in user
+export const saveArt = (ArtData, token) => {
+  return fetch('/api/users', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(ArtData),
+  });
+};
+
+// remove saved book data for a logged in user
+export const deleteBook = (ArtId, token) => {
+  return fetch(`/api/users/books/${ArtId}`, {
+    method: 'DELETE',
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+// make a search to google books api
+// https://www.googleapis.com/books/v1/volumes?q=harry+potter
+export const searchArtPainting = (query) => {
+  return fetch(`https://api.artic.edu/api/v1/artworks/search?q=${query}`);
+};
