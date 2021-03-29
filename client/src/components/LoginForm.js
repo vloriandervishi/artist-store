0f61dd2693c5603ed1d5b2aebbcdab3f2b35e5d8
Lydia-tech <71572534+Lydia-tech@users.noreply.github.com>
2021-03-29 18:32:31 -0500

    added rough draft of the README.md

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,111 +1,92 @@
-import { loginUser } from '../utils/API';
-
-
 // see SignupForm.js for comments
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { useMutation } from '@apollo/react-hooks';
-import { LOGIN_USER } from '../utils/mutations';
-
+import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
-	const [userFormData, setUserFormData] = useState({ email: '', password: '' });
-	const [validated] = useState(false);
-	const [showAlert, setShowAlert] = useState(false);
+  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [validated] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
 
-	const [login, { error }] = useMutation(LOGIN_USER);
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
 
-	useEffect(() => {
-		if (error) {
-			setShowAlert(true);
-		} else {
-			setShowAlert(false);
-		}
-	}, [error]);
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-	const handleInputChange = event => {
-		const { name, value } = event.target;
-		setUserFormData({ ...userFormData, [name]: value });
-	};
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
 
-	const handleFormSubmit = async event => {
-		event.preventDefault();
+    try {
+      const response = await loginUser(userFormData);
 
-		// check if form has everything (as per react-bootstrap docs)
-		const form = event.currentTarget;
-		if (form.checkValidity() === false) {
-			event.preventDefault();
-			event.stopPropagation();
-		}
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
 
-		try {
-			const { data } = await login({
-				variables: { ...userFormData }
-			});
+      const { token, user } = await response.json();
+      console.log(user);
+      Auth.login(token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
 
-			Auth.login(data.login.token);
-		} catch (e) {
-			console.error(e);
-		}
+    setUserFormData({
+      username: '',
+      email: '',
+      password: '',
+    });
+  };
 
-		setUserFormData({
-			email: '',
-			password: ''
-		});
-	};
+  return (
+    <>
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+          Something went wrong with your login credentials!
+        </Alert>
+        <Form.Group>
+          <Form.Label htmlFor='email'>Email</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Your email'
+            name='email'
+            onChange={handleInputChange}
+            value={userFormData.email}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+        </Form.Group>
 
-	return (
-		<>
-			<Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-				<Alert
-					dismissible
-					onClose={() => setShowAlert(false)}
-					show={showAlert}
-					variant='danger'
-				>
-					Something went wrong with your login credentials!
-				</Alert>
-				<Form.Group>
-					<Form.Label htmlFor='email'>Email</Form.Label>
-					<Form.Control
-						type='text'
-						placeholder='Your email'
-						name='email'
-						onChange={handleInputChange}
-						value={userFormData.email}
-						required
-					/>
-					<Form.Control.Feedback type='invalid'>
-						Email is required!
-					</Form.Control.Feedback>
-				</Form.Group>
-
-				<Form.Group>
-					<Form.Label htmlFor='password'>Password</Form.Label>
-					<Form.Control
-						type='password'
-						placeholder='Your password'
-						name='password'
-						onChange={handleInputChange}
-						value={userFormData.password}
-						required
-					/>
-					<Form.Control.Feedback type='invalid'>
-						Password is required!
-					</Form.Control.Feedback>
-				</Form.Group>
-				<Button
-					disabled={!(userFormData.email && userFormData.password)}
-					type='submit'
-					variant='success'
-				>
-					Submit
-				</Button>
-			</Form>
-		</>
-	);
+        <Form.Group>
+          <Form.Label htmlFor='password'>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Your password'
+            name='password'
+            onChange={handleInputChange}
+            value={userFormData.password}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+        </Form.Group>
+        <Button
+          disabled={!(userFormData.email && userFormData.password)}
+          type='submit'
+          variant='success'>
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
 };
 
 export default LoginForm;
