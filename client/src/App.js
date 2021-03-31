589ab8cecff0d8aed59650fe832631f995123a55
Valdo <vlorian22@GMAIL.COM>
2021-03-30 19:24:04 -0500

    updated js files

--- client/src/App.js
+++ client/src/App.js
@@ -28,6 +28,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={searchArt} />
           <Route exact path='/saved' component={savedArts} />
+          
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
       </>
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,13 +2,16 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/react-hooks';
+import { LOGIN_USER } from '../utils/mutations';
+
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,7 +29,7 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const response = await login(userFormData);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
