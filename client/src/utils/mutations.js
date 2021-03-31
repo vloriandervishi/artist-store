8347e11c46b8f20e50c18f087c2e7afe8f85f866
Valdo <vlorian22@GMAIL.COM>
2021-03-30 20:13:53 -0500

    updated mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,25 +1,61 @@
 import gql from 'graphql-tag';
 
 export const LOGIN_USER = gql`
-	mutation login($email: String!, $password: String!) {
-		login(email: $email, password: $password) {
-			token
-			user {
-				_id
-				username
-			}
-		}
-	}
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
 `;
 
 export const ADD_USER = gql`
-	mutation addUser($username: String!, $email: String!, $password: String!) {
-		addUser(username: $username, email: $email, password: $password) {
-			token
-			user {
-				_id
-				username
-			}
-		}
-	}
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_ART = gql`
+  mutation saveart($artData: artInput!) {
+    saveart(artData: $artData) {
+      _id
+      username
+      email
+      savedarts {
+        artId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_ART = gql`
+  mutation removeart($artId: ID!) {
+    removeart(artId: $artId) {
+      _id
+      username
+      email
+      savedarts {
+        artId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
 `;
