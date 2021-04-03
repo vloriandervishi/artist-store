6ac749a5bad3cdfc3290d5d28388b359597a4cfb
Valdo <vlorian22@GMAIL.COM>
2021-04-03 14:53:41 -0500

    reorganized the server file

--- server/server.js
+++ server/server.js
@@ -1,42 +1,36 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-//const cors = require('cors');
-const db = require('./config/connection');
-//require('dotenv').config();
-
 
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
-const app = express();
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
-server.applyMiddleware({ app, path: '/api' });
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json({limit: '2mb'}));
-//app.use(cors());
+app.use(express.json());
 
-
-
-// if we're in production, serve client/build as static assets
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// app.use(routes);
-
 db.once('open', () => {
-  app.listen(PORT, () =>
-   console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
