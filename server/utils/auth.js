05e75f66efee127c7d71ea7698ed8b6949b50059
Valdo <vlorian22@GMAIL.COM>
2021-04-03 14:50:14 -0500

    line 31 added return req wasnt returning token for authorization

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,50 +1,37 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 // set token secret and expiration date
-const secret = 'supersecretToken';
-const expiration = '2h';
+const secret = "supersecretToken";
+const expiration = "2h";
 module.exports = {
   // function for our authenticated routes
   // authMiddleware: function (req, res, next) {
   authMiddleware: function ({ req }) {
+    //  console.log(req.headers,"headers");
     // allows token to be sent via  req.query or headers
-    let token = req.body.token|| req.query.token || req.headers.authorization;
-     //["Bearer", "<tokenvalue>"]
+    let token = req.body.token || req.query.token || req.headers.authorization;
+    //["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token.split(" ").pop().trim();
+      console.log(token, "auth console token");
     }
     if (!token) {
-     // return res.status(400).json({ message: 'You have no token!' });
+      // return res.status(400).json({ message: 'You have no token!' });
+      console.log("token not found");
       return req;
     }
     // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
+      console.log(req.user, "req.user");
     } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      console.log("Invalid token", "auth");
+      // return res.status(400).json({ message: 'invalid token!' });
     }
-    // send to next endpoint
-    next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
