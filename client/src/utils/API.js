ce2a9c0941b32e3104eb4a8b865b84b7eadcbb93
Valdo <vlorian22@GMAIL.COM>
2021-04-02 22:10:18 -0500

    change limit to 100 on api call

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,7 +3,7 @@
 // make a search to google books api
 // https://www.googleapis.com/books/v1/volumes?q=harry+potter
 export const searchArtApi= (query) => {
-  return fetch(`https://api.artic.edu/api/v1/artworks/search?q=${query}&query[term][is_public_domain]=true&limit=20&fields=id,title,image_id,exhibition_history
+  return fetch(`https://api.artic.edu/api/v1/artworks/search?q=${query}&query[term][is_public_domain]=true&limit=100=&fields=id,title,image_id,exhibition_history
 
   `);
   
