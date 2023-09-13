007dbfbd472f1a3f4d73c419f6abce0cd7894887
vlorian <vlorian.dervishi@gmail.com>
2023-09-13 17:08:38 -0400

    updated react

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
-//require('dotenv').config();
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/artpainting', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/artpainting", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
